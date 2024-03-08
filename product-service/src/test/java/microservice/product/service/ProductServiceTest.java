397b41549c55fc38c7b26a592e85a94ca9b83d97
theSiriusDebug <misha.ch2006@gmail.com>
2024-03-08 17:25:18 +0200

    added test for ProductService.

--- product-service/src/test/java/microservice/product/service/ProductServiceTest.java
+++ product-service/src/test/java/microservice/product/service/ProductServiceTest.java
@@ -1,71 +1,69 @@
-//package microservice.product.service;
-//
-//import microservice.product.domain.dto.ProductRegistrationDto;
-//import microservice.product.domain.model.Product;
-//import microservice.product.exception.ValidationException;
-//import microservice.product.infrastructure.validator.ProductValidator;
-//import microservice.product.repository.ProductRepository;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//import org.springframework.validation.BeanPropertyBindingResult;
-//import org.springframework.validation.Errors;
-//
-//import static org.junit.jupiter.api.Assertions.assertThrows;
-//import static org.mockito.Mockito.*;
-//
-//import java.math.BigDecimal;
-//
-//@ExtendWith(MockitoExtension.class)
-//class ProductServiceTest {
-//
-//    @Mock
-//    private ProductRepository repository;
-//
-//    @Mock
-//    private ProductValidator validator;
-//
-//    @InjectMocks
-//    private ProductService service;
-//
-//    private ProductRegistrationDto productDto;
-//
-//    @BeforeEach
-//    void setUp() {
-//        productDto = new ProductRegistrationDto(
-//                "Product Title",
-//                "Product Description",
-//                BigDecimal.valueOf(100.0)
-//        );
-//    }
-//
-//    @Test
-//    void createProduct_ValidProduct_ShouldSaveProduct() throws ValidationException {
-//        // given
-//        Errors errors = new BeanPropertyBindingResult(productDto, "productDto");
-//        when(validator.supports(ProductRegistrationDto.class)).thenReturn(true);
-//        doNothing().when(validator).validate(productDto, errors);
-//
-//        // when
-//        service.createProduct(productDto);
-//
-//        // then
-//        verify(repository, times(1)).save(any(Product.class));
-//    }
-//
-//    @Test
-//    void createProduct_InvalidProduct_ShouldThrowValidationException() throws ValidationException {
-//        // given
-//        Errors errors = new BeanPropertyBindingResult(productDto, "productDto");
-//        when(validator.supports(Product.class)).thenReturn(true);
-//        doThrow(new ValidationException(errors.getAllErrors())).when(validator).validate(productDto, errors);
-//        doThrow(new ValidationException(errors.getAllErrors())).when(repository).save(any(Product.class));
-//
-//        // when and then
-//        assertThrows(ValidationException.class, () -> service.createProduct(productDto));
-//        verify(repository, times(1)).save(any(Product.class));
-//    }
-//}
+package microservice.product.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import microservice.product.domain.dto.ProductRegistrationDto;
+import microservice.product.repository.ProductRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.math.BigDecimal;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@Testcontainers
+@AutoConfigureMockMvc
+class ProductServiceTest {
+
+    @Container
+    static MongoDBContainer container = new MongoDBContainer("mongo:7.0.2");
+
+    private final ProductRepository repository;
+
+    private final MockMvc mvc;
+
+    private final ObjectMapper mapper;
+
+    static {
+        container.start();
+    }
+
+    @Autowired
+    ProductServiceTest(ProductRepository repository, MockMvc mvc, ObjectMapper mapper) {
+        this.repository = repository;
+        this.mvc = mvc;
+        this.mapper = mapper;
+    }
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", container::getReplicaSetUrl);
+    }
+
+    @Test
+    void shouldCreateProduct() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.post("/products/create")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(getProduct())))
+                .andExpect(status().isOk());
+        Assertions.assertEquals(1, repository.findAll().size());
+    }
+
+    private ProductRegistrationDto getProduct() {
+        return ProductRegistrationDto.builder()
+                .title("title")
+                .description("description")
+                .price(BigDecimal.valueOf(12321))
+                .build();
+    }
+}
